130009164d5aa2afcad82867070277566c3eaeef
evsecode <mynameisevse@gmail.com>
2023-12-15 02:26:38 +0300

    created app prototype(Available users status, users table)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "bootstrap/dist/css/bootstrap.css"
+
 
 import reportWebVitals from './reportWebVitals';
 import Users from "./components/Users";
