4e030c97267c43874f3b876cf8c96017644756fa
evsecode <mynameisevse@gmail.com>
2023-12-17 03:19:06 +0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import "bootstrap/dist/css/bootstrap.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
+import reportWebVitals from "./reportWebVitals";
+import App from "./app";
 
-import reportWebVitals from './reportWebVitals';
-import Users from "./components/Users";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Users />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
