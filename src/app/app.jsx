9952ce965fb5a7769bcc9642f7c0170b587bcfef
evsecode <mynameisevse@gmail.com>
2024-04-10 05:34:50 +0300

    changed local database method to firebase realisation. Added mini-app "initialize db" for sending local db content to firebase realtime database.

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
 import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
+import { ProfessionProvider } from "./hooks/useProfession";
+import { QualitiesProvider } from "./hooks/useQualities";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Routes>
-        <Route path="/users/:userId?/:edit?" element={<Users />} />
-        <Route path="/login/:type?" element={<Login />} />
-        <Route path="/" element={<Main />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <QualitiesProvider>
+        <ProfessionProvider>
+          <Routes>
+            <Route path="/users/:userId?/:edit?" element={<Users />} />
+            <Route path="/login/:type?" element={<Login />} />
+            <Route path="/" element={<Main />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </ProfessionProvider>
+      </QualitiesProvider>
+      <ToastContainer />
     </div>
   );
 }
